5e06d1d765060b74b3c28bc3118936155d640a57
Karan Kumar <karankumar072508@gmail.com>
2023-01-24 14:23:32 +0530

    adding initials files

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Karan Kumar!! 🚀 🥪 🙂 🙋   </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
